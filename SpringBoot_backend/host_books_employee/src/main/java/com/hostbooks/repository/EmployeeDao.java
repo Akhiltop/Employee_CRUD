eafc7e606468dac20936f9792883a9bbd120efc5
Akhiltop <akhilyadlapalli@gmail.com>
2023-06-23 10:41:54 +0530

    added springboot backend application

---
+++ SpringBoot_backend/host_books_employee/src/main/java/com/hostbooks/repository/EmployeeDao.java
@@ -0,0 +1,13 @@
+package com.hostbooks.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hostbooks.model.Employee;
+
+public interface EmployeeDao extends JpaRepository<Employee, Integer>{
+
+	
+	
+	
+	
+}
